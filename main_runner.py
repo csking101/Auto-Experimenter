953fd4f0c6efd1e088caa94a7a5731fe23fc819b
csking101 <42250012+csking101@users.noreply.github.com>
2023-09-10 12:44:03 +0530

    Added Train and Test functionality

--- main_runner.py
+++ main_runner.py
@@ -4,15 +4,16 @@ from training.trainer import CNNTrainer
 from experiments.experimenter import Experiment
 from testing.tester import MNISTAccuracyTester
 
-data_loader = CustomDataLoader()#Need to differentiate between train and test
+train_data_loader = CustomDataLoader(train=True)
+test_data_loader = CustomDataLoader(train=False)
 print("Data Loader made")
 model = SimpleCNN()
 print("Model made")
 experiment = Experiment(learning_rate=0.001,num_epochs=1)
 print("Experiment made")
-trainer = CNNTrainer(model,data_loader,experiment)
+trainer = CNNTrainer(model,train_data_loader,experiment)
 print("Trainer made")
 trainer.train()
-tester = MNISTAccuracyTester(model,data_loader)
+tester = MNISTAccuracyTester(model,test_data_loader)
 print("Tester made")
 tester.test_accuracy()
